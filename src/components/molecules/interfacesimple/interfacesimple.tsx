9baa88f8fb4d2bcc5045208063678a91f450e639
Christian Albarracin <christianalex-071@hotmail.com>
2022-12-16 13:27:36 -0500

    commit 5

--- src/components/molecules/interfacesimple/interfacesimple.tsx
+++ src/components/molecules/interfacesimple/interfacesimple.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Label from "../../atoms/label/label";
 import Button from "../../atoms/button/button";
 import Input from "../../atoms/input/input";
+import clienHttp from "../../../services/client/client";
 
 const InterfaceSimple = () => {
+
+    const [url, setUrl] = useState("");
+    const object ={ "url":`"${url}"`, "author_id":34};
+    const Agregar=()=>{
+        console.log("presionado");
+        clienHttp.post("",object)
+            .then((respose)=>{
+                console.log(respose.data)
+            })
+
+    }
+
+    const OnChange=()=>{
+        setUrl(event.target.value);
+    }
+
     return (
         <div>
             <Input
                 value="test"
                 name="addGifs"
-                onChange={()=>console.log("tipeando..")}
+                onChange={()=>OnChange()}
                 placeHolder="Gift URL"/>
 
-            <Button onClick={()=>console.log("presionado")} children="Agregar" size="small"/>
+            <Button onClick={()=>Agregar()} children="Agregar" size="small"/>
         </div>
     );
 };
