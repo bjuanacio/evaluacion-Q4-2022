75617bc1a9aff4f007b80ae211e77bd025f0143b
Juan Castillo <juacasti@pichincha.com>
2022-12-16 13:49:58 -0500

    control de lista vacia

--- src/components/molecules/empty-message/index.tsx
+++ src/components/molecules/empty-message/index.tsx
@@ -1,8 +1,9 @@
 import WarningIcon from "../../../assets/warning-icon.svg"
+import "./empty-message.scss"
 
 export const EmptyMessage = () => {
-  return <div className="app--empty-list">
-    <img className="app__icon" src={WarningIcon} alt="Warning icon" />
-    <span className="app__message">No posee Gifs</span>
+  return <div className="message--container">
+    <img className="message__icon" src={WarningIcon} alt="Warning icon" />
+    <span className="message__text">No posee Gifs</span>
   </div>
 }
