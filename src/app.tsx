79dbd2e9e0c769f58e7592f99a8198673484f2b8
BPUIO01610041LM <bpuio01610041lm@BPUIO01610041LM.local>
2022-12-19 09:20:45 -0500

    Revert "Merge pull request #21 from carlospuco/cpucolog"
    This reverts commit 9a41a347830cb6ab1555cff83cecfca4fd99a07c, reversing
    changes made to 07cfcd92ee8117d62254e3010e7fbae7ee9103aa.

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,61 @@ import WarningIcon from "./assets/warning-icon.svg";
 
 import "./app.scss";
 
-import { Button } from "./components/atoms/button/button";
-import { Layer } from "./components/atoms/layer/layer";
-import { Submit } from "./components/molecules/submit";
-
 const App = () => {
   return (
-    <>
-     <Submit></Submit>
-    </>
+    <div className="app">
+      <h1>Evaluación Técnica Q4 2022</h1>
+      <section>
+        <h2>Colores</h2>
+        <div className="app__color-container">
+          <span className="app__color-title">Principal</span>
+          <div className="app__color app__color--main"></div>
+          <code>#B234B1</code>
+        </div>
+        <div className="app__color-container">
+          <span className="app__color-title">Fondo</span>
+          <div className="app__color app__color--background"></div>
+          <code>#1C1C1C</code>
+        </div>
+      </section>
+      <section>
+        <h2>Íconos</h2>
+        <div className="app__icon-container">
+          <div className="app__icon-description">
+            <img className="app__icon" src={DeleteIcon} alt="Delete icon" />
+            <span>Delete icon</span>
+          </div>
+          <div className="app__icon-description">
+            <img className="app__icon" src={WarningIcon} alt="Warning icon" />
+            <span className="app__color-title">Warning icon</span>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2>API REST</h2>
+        <div>
+          <a
+            href="https://documenter.getpostman.com/view/21329689/2s8YsnYwZr"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Documentación de la API REST
+          </a>
+        </div>
+      </section>
+      <section>
+        <h2>GIFs</h2>
+        <div>
+          <a
+            href="https://tenor.com/es-419/search/avengers-gifs"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Página de GIFs
+          </a>
+        </div>
+      </section>
+    </div>
   );
 };
 
